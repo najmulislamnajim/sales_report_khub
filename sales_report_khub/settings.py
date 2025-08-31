e550cfe006470c782a0f77fd6b9f3d46f6d2d6b7
najmulislamnajim <ninbd17@gmail.com>
2025-08-31 10:09:18 +0600

    add reports app

--- sales_report_khub/settings.py
+++ sales_report_khub/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'rest_framework',
     
     'myuser',
+    'reports_app',
 ]
 
 MIDDLEWARE = [
--- sales_report_khub/urls.py
+++ sales_report_khub/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/user/',include('myuser.urls')),
+    path('api/v1/reports/',include('reports_app.urls')),
 ]
