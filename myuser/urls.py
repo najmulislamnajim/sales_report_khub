d2ef88c4effe68257255c196587b749f04563bba
najmulislamnajim <ninbd17@gmail.com>
2025-09-03 14:41:32 +0600

    get next user list api

--- myuser/urls.py
+++ myuser/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('login',LoginView.as_view(), name='login'),
     path('brands',GetBrands.as_view(), name='get_brands'),
+    path('next-user-list',GetNextUserList.as_view(), name='get_next_user_list'),
 ]
--- myuser/views.py
+++ myuser/views.py
@@ -64,3 +64,46 @@ class GetBrands(APIView):
             status=status.HTTP_200_OK
         )
         
+class GetNextUserList(APIView):
+    def get(self, request):
+        work_area_t = request.query_params.get('work_area_t')
+        designation_id = int(request.query_params.get('designation_id'))
+        designation_mapping = {
+            1: "work_area_t",
+            2: "rm_code",
+            3: "zm_code",
+            4: "sm_code",
+            5: "gm_code"
+        }
+        next_designation_id = max(designation_id - 1, 1)
+        query = f"""
+            SELECT
+                ul.work_area_t,
+                ul.name
+            FROM rpl_user_list ul 
+            WHERE ul.{designation_mapping[designation_id]} = %s AND ul.designation_id = %s;
+        """
+        print(work_area_t, designation_id, next_designation_id)
+        with connection.cursor() as cursor:
+            cursor.execute(query, [work_area_t, next_designation_id])
+            rows = cursor.fetchall()
+
+        if not rows:
+            return Response(
+                {"success": False, "message": "No data found"},
+                status=status.HTTP_404_NOT_FOUND
+            )
+        
+        next_user_list = [f"{row[0]} - {row[1]}" for row in rows]
+        data = {
+            "work_area_t": work_area_t,
+            "current_designation_id": designation_id,
+            "user_list": next_user_list,
+            "next_designation_id": next_designation_id
+        }
+
+        return Response(
+            {"success": True, "message": "Next user list fetched successfully.", "data": data},
+            status=status.HTTP_200_OK
+        )
+        
